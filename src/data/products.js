0e161ed8e5f37bb93a7ad7ef2ebbd0291c53058a
Edgar <57589251+Edgar-MP@users.noreply.github.com>
2023-07-06 21:25:17 +0200

    First Commit

---
+++ src/data/products.js
@@ -0,0 +1,43 @@
+export const products = [
+    {
+        id: 'prendas-exteriores',
+        title: 'Prendas Exteriores',
+        img: 'https://www.ternuagroup.com/site/assets/files/1116/segundas-capas-1.png',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
+        price: '10€'
+    },
+    {
+        id: 'segundas-camisas',
+        title: 'Segundas y Camisas',
+        img: 'https://www.ternuagroup.com/site/assets/files/1117/segundas-capas-1.png',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
+        price: '10€'
+    },
+    {
+        id: 'polos-camisas',
+        title: 'Polos y Camisas',
+        img: 'https://www.ternuagroup.com/site/assets/files/1118/polos-y-camisas-1.png',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
+        price: '10€'
+    },
+    {
+        id: 'pantalones',
+        title: 'Pantalones',
+        img: 'https://www.ternuagroup.com/site/assets/files/1119/pantalones.png',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
+        price: '10€'
+    },
+    {
+        id: 'baselayers',
+        title: 'Baselayers',
+        img: 'https://www.ternuagroup.com/site/assets/files/1120/baselayers-1.png',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
+        price: '10€'
+    },
+    // {
+    //     id: 'calcetines',
+    //     title: 'Calcetines',
+    //     img: 'https://www.ternuagroup.com/site/assets/files/1121/calcetines-1.png',
+    //     desc: 'C3WW T3 WORK WINTER'
+    // },
+]
